e077ec72f753aba09067c7c990605722598beba9
pkettman <pkettman@redhat.com>
2018-04-12 14:30:49 +0000

    Budapest

--- src/main/java/io/openshift/booster/HttpApplication.java
+++ src/main/java/io/openshift/booster/HttpApplication.java
@@ -11,7 +11,7 @@ import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 public class HttpApplication extends AbstractVerticle {
 
-  static final String template = "Hello from Bratislava, %s!";
+  static final String template = "Hello from Budapest, %s!";
 
   @Override
   public void start(Future<Void> future) {
